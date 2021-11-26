307bb678cd8ef28710d96b4f2a03211af2997939
Jimin LEE <wlals2997@naver.com>
2021-11-27 01:19:28 +0900

    Casino number game
    다른 방식으로 풀어본것

--- test4/test4.js
+++ test4/test4.js
@@ -1,25 +1,28 @@
-const wholeNumber = document.querySelector("#whole-number input");
-console.log(wholeNumber)
+const wholeNumber = document.querySelector("#whole-number whole");
 const yourInput = document.querySelector("#your-number guess");
 const yourButton = document.querySelector("#your-number button");
 const resultNumber = document.querySelector("#result-num");
 const resultCondition = document.querySelector("#result-con");
+const result=document.querySelector('#result')
 const HIDDEN_CLASSNAME = "hidden";
 function numberSubmit(event) {
+    const numberRange = whole.value
+    const yourNumber = guess.value
+    const machineNumber = Math.ceil(Math.random()* numberRange)
+     console.log(typeof(yourNumber))
+     console.log(typeof(numberRange))
     event.preventDefault()
-    const yourNumber = wholeNumber.value
-    console.log(yourNumber)
-    // const machineNumber = Math.ceil(Math.random()* yourNumber)
-  
-    // localStorage.setItem("number",machineNumber)
-    // resultNumber.innerText = `You chose: ${yourNumber} , the machine chose: ${machineNumber}.`
-    // resultNumber.classList.remove(HIDDEN_CLASSNAME)
-    // if (yourNumber == machineNumber) {
-    //     resultCondition.innerText = "You won!"
-    //  } else (yourNumber != machineNumber); {
-    //      resultCondition.innerText = "You lost!"
-    //  }
-    //  resultCondition.classList.remove(HIDDEN_CLASSNAME);
-}
+    localStorage.setItem("number",machineNumber)
+    result.innerText = `You chose: ${yourNumber} , the machine chose: ${machineNumber}.`
+    
+    if (yourNumber == machineNumber) {
+        resultNumber.classList.remove(HIDDEN_CLASSNAME)
+        resultCondition.classList.add(HIDDEN_CLASSNAME)
+        resultNumber.innerHTML = 'You won!';
+      } else {
+        resultNumber.classList.add(HIDDEN_CLASSNAME)
+        resultCondition.classList.remove(HIDDEN_CLASSNAME);
+        resultCondition.innerHTML = 'You Lost!';
+      }
+    }
 yourButton.addEventListener("click", numberSubmit);
-// const savedNumber = localStorage.getItem("number");
